94aad47c56fd007ff23db288d6590ab87ab5a25c
Moni Monroe <maguado85@gmail.com>
2018-03-15 16:58:59 +0100

    pokemon's list with failed in fetch and try router

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,76 @@
 import React, { Component } from 'react';
+import Pokemon from './Pokemon';
+import Details from './Details';
 import '../App.css';
+
 
 import pokedex from './../images/Pokedex_logo.png';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.handleSearch = this.handleSearch.bind(this);
+
+    this.state = {
+      pokemons: [],
+      inputPoke: ''
+    }
+  }
+
+  componentDidMount () {
+    for (var i = 1; i < 26; i++) {
+      fetch(`http://pokeapi.co/api/v2/pokemon/${i}/`)
+      .then(response => response.json())
+      .then(json => {
+        this.setState({
+          pokemons: this.state.pokemons.push([json])
+        })
+      });
+  }
+};
+
+handleSearch(e) {
+  const valueInput = e.target.value.toLowerCase();
+
+  this.setState({
+    inputPoke: valueInput
+  })
+}
+
+paintPokemon(){
+  let listPokemon = this.state.pokemons.filter(pokemon => pokemon.name.toLowerCase()
+  .includes(this.state.inputPoke));
+
+  return(
+    <ul className="pokemon__list">
+
+      {listPokemon.sort(function(a,b){
+      return a.id - b.id;}).map(
+        (pokemon) =>
+          <li key = {pokemon.id}>
+            <Pokemon
+              id = {pokemon.id}
+              name = {pokemon.name}
+              types = {pokemon.types.sort((typeNumber) => typeNumber.slot).map((typeNumber) => typeNumber.type.name)}
+              image={pokemon.sprites.front_default}/>
+          </li>
+      )
+      }
+    </ul>
+  )
+}
   render() {
     return (
       <div className="app">
         <header className="header">
           <img src={pokedex} className="header__logo" alt="logo" />
-          <h1 className="header__title">Welcome to PokeDex</h1>
         </header>
         <main className="main">
           <div>
-            <h2 className="main__text">Search your favorite Pokemon in our pokeDex</h2>
-            <input className="main__filter" placeholder="Search the Pokemon"></input>
+            <h2 className="main__text">Search your favorite Pokemon in our PokeDex</h2>
+            <input className="main__filter" placeholder="Search the Pokemon" value={this.state.valueInput} onChange={this.state.handleSearch}></input>
+            {this.paintPokemon()}
           </div>
           </main>
 
---
+++ src/components/Pokemon.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+class Pokemon extends React.Component{
+  render(){
+    return(
+      <div className="pokemon__container">
+        <div className="pokemon__image--container">
+          <img className="pokemon__image" src={this.props.image} alt= {this.props.name}/>
+        </div>
+        <div className="pokemon__data">
+          <h2 className="pokemon__title"><span className="pokemon__number">Nº {this.props.id}</span>
+            <span className="pokemon__name">{this.props.name}</span> </h2>
+          <ul className="pokemon__types--list">
+            {this.props.types.map((type, index) =>
+              <li key={index} className={`pokemon__type ${type}`}>
+                {type}
+              </li>)}
+          </ul>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Pokemon;
